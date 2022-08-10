c08825e9dd8797de5b5dd11fc0709877f95ccf5d
Artur Harutyunyan <Arturhar@outlook.com>
2022-08-10 21:52:31 +0400

    pwa

--- sw.js
+++ sw.js
@@ -1,20 +1,48 @@
-let cacheName = "Tic Tac Toe";
-let filesToCache = ["/", "./index.html", "./style.css", "./script.js"];
+importScripts('assets/lib/cache-polyfill.js');
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-});
+let CACHE_VERSION = 'app-v0.00';
+// give all files path you want to work offline
+let CACHE_FILES = [
+  './',
+  'index.html',
+  'style.css',
+  'script.js',
+  'img/logo-256.png',
+  'img/logo-512.png'
+];
 
-/* Serve cached content when offline */
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
-});
+self.addEventListener('install', function (event) {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_VERSION)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(CACHE_FILES);
+            })
+    )
+})
+
+self.addEventListener('fetch', function (event) {
+    let online = navigator.onLine
+    if (!online) {
+        event.respondWith(
+            caches.match(event.request).then(function (res) {
+                if (res) {
+                    return res;
+                }
+            })
+        )
+    }
+})
+
+self.addEventListener('activate', function(event){
+    event.waitUntil(
+        caches.keys().then(function(keys){
+            return prompt.all(keys.map(function(keys, i){
+                if(keys !== CACHE_VERSION){
+                    return caches.delete(keys[i]);
+                }
+            }))
+        })
+    )
+})
