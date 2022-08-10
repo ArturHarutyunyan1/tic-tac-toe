9665a2d718e3603d236a1ce26d08297faf92cf16
Artur Harutyunyan <Arturhar@outlook.com>
2022-08-11 01:24:02 +0400

    pwa

--- src/js/script.js
+++ src/js/script.js
@@ -198,33 +198,3 @@ if(oValue){
 if(tieValue){
     ties.innerHTML = tie = JSON.parse(localStorage.getItem('ties', tie))
 }
-
-if('serviceWorker' in navigator){
-    navigator.serviceWorker
-    // give complete path of sw.js file
-    .register('https://arturharutyunyan1.github.io/tic-tac-toe/sw.js')
-    .then(() => console.log('Service Worker Registered'))
-}
-
-let deferredPromt;
-const addBtn = document.querySelector('.installApp')
-addBtn.style.display = 'none'
-
-window.addEventListener('beforeinstallprompt', (e) => {
-    e.preventDefault();
-    deferredPromt = e;
-    addBtn.style.display = 'block'
-
-    addBtn.addEventListener('click', () => {
-        addBtn.style.display = 'none';
-        deferredPromt.prompt();
-        deferredPromt.userChoice.then((choiceResult) => {
-            if(choiceResult.outcome === 'accepted'){
-                console.log('App is installing');
-            }else{
-                console.log('User dismissed the prompt');
-            }
-            deferredPromt = null;
-        })
-    })
-})
